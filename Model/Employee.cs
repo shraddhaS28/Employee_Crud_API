78163dc1e501626b43b724fc471cdd4e11429199
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-24 17:29:54 +0530

    [Shraddha_Singh] Add. Added employee class, which is model class to get employee data

---
+++ Model/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model
+{
+    public class Employee
+    {
+        private string email;
+        private string password;
+        private int empId;
+        private string mobile;
+        private string address;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int EmpId
+        {
+            get
+            {
+                return this.empId;
+            }
+            set
+            {
+                this.empId = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                this.password = value;
+            }
+        }
+        public string Mobile
+        {
+            get
+            {
+                return this.mobile;
+            }
+            set
+            {
+                this.mobile = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return this.address;
+            }
+            set
+            {
+                this.address = value;
+            }
+        }
+    }
+}
