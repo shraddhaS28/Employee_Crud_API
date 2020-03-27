c89c7ead1e130eb2690ac4350c49b37989268f8c
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-27 18:03:57 +0530

    [Shraddha_Singh] Refactored. Written code for CRUD operation in controller

--- EmployeeManagementSystem/Controllers/ValuesController.cs
+++ EmployeeManagementSystem/Controllers/ValuesController.cs
@@ -1,45 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace EmployeeManagementSystem.Controllers
+﻿namespace EmployeeManagementSystem.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Manager;
+    using Microsoft.AspNetCore.Mvc;
+    using Model;
+
+    /// <summary>
+    /// Controller class.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        /// <summary>
+        /// variable of imanager interface type.
+        /// </summary>
+        public readonly IManager Manager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValuesController"/> class.
+        /// Parameerized Constructor.
+        /// </summary>
+        /// <param name="manager"></param>
+        public ValuesController(IManager manager)
         {
-            return new string[] { "value1", "value2" };
+            this.Manager = manager;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        /// <summary>
+        /// method to add employee data.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns>  Updated Employee Data </returns>
+        [Route("AddEmployee")]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> AddEmployee(Model.EmployeeModel employee)
         {
+            var result = await this.Manager.AddEmployee(employee);
+            if (result == 1)
+            {
+                return this.Ok(employee);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        /// <summary>
+        /// method to get all employees data.
+        /// </summary>
+        /// <returns> Updated Employee Data </returns>
+        [Route("GetAllEmployee")]
+        [HttpGet]
+        public IEnumerable<EmployeeModel> GetAllEmployees()
         {
+            return this.Manager.GetAllEmployees();
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        /// <summary>
+        /// method to update employee data.
+        /// </summary>
+        /// <param name="employeeChanges"></param>
+        /// <returns> Updated Employee Data </returns>
+        [Route("UpdateEmployee")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(EmployeeModel employeeChanges)
         {
+            var result = await this.Manager.UpdateEmployee(employeeChanges);
+            if (result == 1)
+            {
+                return this.Ok(employeeChanges);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// method to delete from employee data.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Updated Employee Data </returns>
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public EmployeeModel DeleteEmployee(int id)
+        {
+            return this.Manager.DeleteEmployee(id);
+        }
+
+        /// <summary>
+        /// method to get employee data.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Updated Employee Data </returns>
+        [Route("GetEmployee")]
+        [HttpGet]
+        public EmployeeModel GetEmployee(int id)
+        {
+            return this.Manager.GetEmployee(id);
         }
     }
 }
--- EmployeeManagementSystem/Startup.cs
+++ EmployeeManagementSystem/Startup.cs
@@ -1,42 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Repository.UserContext;
-using Swashbuckle.AspNetCore.Swagger;
-
-namespace EmployeeManagementSystem
+﻿namespace EmployeeManagementSystem
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Manager;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using Repository.IRepository;
+    using Repository.RepoImplementation;
+    using Repository.UserContext;
+    using Swashbuckle.AspNetCore.Swagger;
+
+    /// <summary>
+    /// Startup Class.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets represent set of key value application.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContextPool<UserDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
+            services.AddDbContextPool<UserDbContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("UserDbConnection")));
+            services.AddTransient<IManager, MngrImplementation>();
+            services.AddTransient<IRepo, RepoImplement>();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title="EmployeeManagementSystem", Version="v1" });
+                c.SwaggerDoc("v1", new Info { Title = "EmployeeManagementSystem", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -47,7 +69,7 @@ namespace EmployeeManagementSystem
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyApi V1");
                 });
-            } 
+            }
             else
             {
                 app.UseHsts();
