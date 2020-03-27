f5a3f06a2cd774cacf5703c6432fb284dc475287
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-27 10:52:38 +0530

    [Shraddha_Singh] Add. Added inteerface implementation class in repository

---
+++ Repository/RepoImplementation/RepoImplement.cs
@@ -0,0 +1,55 @@
+﻿using Model;
+using Repository.IRepository;
+using Repository.UserContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepoImplementation
+{
+    public class RepoImplement : IRepo
+    {
+        private readonly UserDbContext userDBContext;
+
+        public RepoImplement(UserDbContext userDBContext)
+        {
+            this.userDBContext = userDBContext;
+        }
+        public EmployeeModel AddEmployee(EmployeeModel employee)
+        {
+            userDBContext.Employees.Add(employee);
+            userDBContext.SaveChanges();
+            return employee;
+        }
+
+        public EmployeeModel DeleteEmployee(int id)
+        {
+            EmployeeModel employee = userDBContext.Employees.Find(id);
+            if (employee != null)
+            {
+                userDBContext.Employees.Remove(employee);
+                userDBContext.SaveChanges();
+            }
+            return employee;
+
+        }
+
+        public IEnumerable<EmployeeModel> GetAllEmployees()
+        {
+            return userDBContext.Employees;
+        }
+
+        public EmployeeModel GetEmployee(int id)
+        {
+            return userDBContext.Employees.Find(id);
+        }
+
+        public EmployeeModel UpdateEmployee(EmployeeModel employeeChanges)
+        {
+            var employee = userDBContext.Employees.Attach(employeeChanges);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            userDBContext.SaveChanges();
+            return employeeChanges;
+        }
+    }
+}
