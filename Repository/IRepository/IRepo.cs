bd266551a8483cd0bb02b30d814b731952eb4f88
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-27 10:51:49 +0530

    [Shraddha_Singh] Add. Added interface in repository

---
+++ Repository/IRepository/IRepo.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.IRepository
+{
+    public interface IRepo
+    {
+        EmployeeModel GetEmployee(int id);
+        IEnumerable<EmployeeModel> GetAllEmployees();
+        EmployeeModel AddEmployee(EmployeeModel employee);
+        EmployeeModel UpdateEmployee(EmployeeModel employeeChanges);
+        EmployeeModel DeleteEmployee(int id);
+    }
+}
