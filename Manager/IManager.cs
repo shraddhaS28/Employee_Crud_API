ec18184766d2c8e22076ab1af86062cb9360da37
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-27 16:22:00 +0530

    [Shraddha_Singh] Add. Added interface manager

---
+++ Manager/IManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Manager
+{
+    using Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Manager Interface 
+    /// </summary>
+    public interface IManager
+    {
+        EmployeeModel GetEmployee(int id);
+        IEnumerable<EmployeeModel> GetAllEmployees();
+        Task<int> AddEmployee(EmployeeModel employee);
+        Task<int> UpdateEmployee(EmployeeModel employeeChanges);
+        EmployeeModel DeleteEmployee(int id);
+    }
+}
