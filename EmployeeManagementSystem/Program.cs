88115ea7b32caf48303b1e871f2ce7ac650651aa
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-03-29 23:05:54 +0530

    [Shraddha_Singh] Refactored. Implemented Logger in the Project.

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -1,22 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace EmployeeManagementSystem
+﻿namespace EmployeeManagementSystem
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EmployeeManagementSystem.Controllers;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+
+    /// <summary>
+    /// Program file of EmployeeManagement System.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// main method of the class.
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(@"C:\Users\bittu\source\repos\Employee_Crud_API\EmployeeManagementSystem\Controllers\LogFile.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
         }
 
+        /// <summary>
+        /// method to call startup class.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns> Employeee data</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
