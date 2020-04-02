6dc0cdf9be43ca765aa8e92861bbed6f3a5b0e24
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 00:21:50 +0530

    [Shraddha_Singh] Add. Added Inch.cs class to repository

---
+++ QuantityMeasurement/Inch.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Inch.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shraddha Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace QuantityMeasurement
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Inch Class
+    /// </summary>
+    class Inch
+    {
+        /// <summary>
+        /// private value of inch
+        /// </summary>
+        private int inchValue;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Inch()
+        {
+        }
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="inchValue"></param>
+        public Inch(int inchValue)
+        {
+            this.inchValue = inchValue;
+        }
+
+        /// <summary>
+        /// Method to get inch value
+        /// </summary>
+        /// <returns></returns>
+        public int EqualisVaueEqualValue()
+        {
+            return this.inchValue;
+        }
+
+        /// <summary>
+        /// Overridden equals method
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return true;
+
+            return false;
+        }
+    }
+}
