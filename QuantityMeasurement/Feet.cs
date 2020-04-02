48a697b810931ec84d5749670be542d1b5765664
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-02 22:07:50 +0530

    [Shraddha_Singh] Refactored. written a constructor and overriden equals method

--- QuantityMeasurement/Feet.cs
+++ QuantityMeasurement/Feet.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace QuantityMeasurement
+﻿namespace QuantityMeasurement
 {
-    class Feet
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Feet Class 
+    /// </summary>
+    public class Feet
     {
+        /// <summary>
+        /// value of feet parameter
+        /// </summary>
+        private int feetValue;
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="feetValue"></param>
+        public Feet(int feetValue)
+        {
+            this.feetValue = feetValue;
+        }
+
+        /// <summary>
+        /// Overridden equals method
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (feetValue.Equals(obj))
+                return true;
+            else
+                return false;
+
+        }
     }
 }
