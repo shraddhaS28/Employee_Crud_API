908e1a9f03b97d4e833da013891388ccd6affc7d
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 20:11:31 +0530

    [Shraddha_Singh] Add. Added class Gram.cs , overridden equals method to check null object

---
+++ QuantityMeasurement/Gram.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Gram.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shraddha Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuantityMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Gram class
+    /// </summary>
+    public class Gram
+    {
+        /// <summary>
+        /// Overridden Equals method
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>True or false</returns>
+        public override bool Equals(object obj)
+        {
+            if ((obj == null))
+                return true;
+            return false;
+        }
+    }
+}
