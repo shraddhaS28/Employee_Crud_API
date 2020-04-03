fba6f97678c4fc03d3a73c8529c3355c07a3ba2f
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 23:02:26 +0530

    [Shraddha_Singh] Refactored. written method to return fahrenheit value

--- QuantityMeasurement/Fahrenheit.cs
+++ QuantityMeasurement/Fahrenheit.cs
@@ -1,11 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Fahrenheit.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shraddha Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace QuantityMeasurement
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    
+    /// <summary>
+    /// fahrenheit class
+    /// </summary>
     public class Fahrenheit
     {
+        /// <summary>
+        /// private parameter
+        /// </summary>
+        private double ftValue;
+
+        /// <summary>
+        /// Method to get gram value
+        /// </summary>
+        /// <param name="ftValue"></param>
+        /// <returns> double value </returns>
+        public double getValue(double ftValue)
+        {
+            this.ftValue = ftValue;
+            return this.ftValue;
+        }
+
         /// <summary>
         /// Overridden Equals method
         /// </summary>
