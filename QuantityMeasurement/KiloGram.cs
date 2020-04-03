caed5b224568b0ac889fc3048588748a1607a4ef
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 19:56:58 +0530

    [Shraddha_Singh] Refactored. written method to return kilogram value

--- QuantityMeasurement/KiloGram.cs
+++ QuantityMeasurement/KiloGram.cs
@@ -17,8 +17,27 @@ namespace QuantityMeasurement
     /// </summary>
     public class KiloGram
     {
+        /// <summary>
+        /// private parameter
+        /// </summary>
         private int kgValue;
 
+        /// <summary>
+        /// method to get kilogram value.
+        /// </summary>
+        /// <param name="kgValue"></param>
+        /// <returns></returns>
+        public int getValue(int kgValue)
+        {
+            this.kgValue = kgValue;
+            return this.kgValue;
+        }
+
+        /// <summary>
+        /// Overridden Equals method
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if( (obj == null) || object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()) )
