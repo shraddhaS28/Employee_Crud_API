eb5ffa1deccba9e53aaebd61b149b0c3de4c28c9
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 18:06:50 +0530

    [Shraddha_Singh] Add. Added class kilogram.cs , overridden equals method to check null object

---
+++ QuantityMeasurement/KiloGram.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KiloGram.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shraddha Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuantityMeasurement
+{
+    
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// kilogram class
+    /// </summary>
+    public class KiloGram
+    {
+        private int kgValue;
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null)
+                return true;
+            return false;
+        }
+    }
+}
