1f3c396cbab6bdfc69ce0b3f51facb8734ae04b7
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-03 17:34:06 +0530

    [Shraddha_Singh] Add. Added class for measurement conversion, written method to convert inch value into feet

---
+++ QuantityMeasurement/MeasurementConversion.cs
@@ -0,0 +1,23 @@
+﻿namespace QuantityMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// conversion class conains method to convert measurements from one type to other
+    /// </summary>
+    public class MeasurementConversion
+    {
+        private int value;
+        /// <summary>
+        /// Method to conver value from feet to inch type 
+        /// </summary>
+        /// <returns></returns>
+        public int ConvertFromInchToFeet(int value)
+        {
+            this.value = value;
+            return this.value / 12;
+        }
+    }
+}
