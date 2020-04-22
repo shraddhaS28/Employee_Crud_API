63f8976d31fabec9daaa1a95f2515863ac59890a
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-22 19:42:59 +0530

    [Shraddha_Singh] Refactored. Correction of code to get Meter-Centimeter typemeasument formula

--- qmreactapplication/src/Components/RightBox.js
+++ qmreactapplication/src/Components/RightBox.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-class InputTypeRight extends Component{
+class RightBox extends Component{
     render(){
         return(
             <div className='col-md m-5' id='main-label'>
-                <input type='text' className="form-control form-control-md" onChange={this.props.inputTypeRight} value={this.props.valueRight} />
+                <input type='text' className="form-control form-control-md" onChange={this.props.RightBox} value={this.props.valueRight} />
                 <select name="measurementType" id="measurementType" className='form-control'  onChange={this.props.selectTypeRight}>
-                <option value="-1" selected>Select</option>
+                <option value="-1" selected>Select</option>``
                 
                 {
                     this.props.showType?
                     <>
                 <option name="length">Feet</option>
                 <option name="weight">Inch</option>
-                <option name="weight">meter</option>
-                <option name="weight">centimeter</option></> : <>
+                <option name="weight">Meter</option>
+                <option name="weight">Centimeter</option></> : <>
                 <option name="weight">Kilogram</option>
                 <option name="weight">Gram</option>
                 </>
@@ -23,4 +23,4 @@ class InputTypeRight extends Component{
         )
     }
 }
-export default InputTypeRight;
+export default RightBox;
