77176a63cd1bf3dda9814139304139687fddd17c
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-22 20:22:12 +0530

    [Shraddha_Singh] Refactored. Completed Quantity Measurement UI

--- qmreactapplication/src/Components/MeasureMentUI.js
+++ qmreactapplication/src/Components/MeasureMentUI.js
@@ -4,7 +4,7 @@ class MeasurementUI extends Component {
     render() {
         return (
             <select name="MeasurementUI" id="measurementType" className=' form-control'  onChange={this.props.measurementTypeChanged}>
-                <option value="-1" selected>Select</option>
+                <option selected>Measurement Type</option>
                 <option name="length">Length</option>
                 <option name="weight">Weight</option>
             </select>
