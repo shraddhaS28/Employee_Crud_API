2d361e96e6cdcb2875bf8b5a204045c070784ed2
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-28 13:14:20 +0530

    [Shraddha_Singh] Refactored. Given route to main page

--- fundoonotesui/src/App.js
+++ fundoonotesui/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import MainPage from './Components/MainPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' component={MainPage} exact />
+        </Switch>
+      </Router>
+    )
+  }
 }
-
 export default App;
