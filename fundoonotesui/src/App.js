fbc3590f18391012e2d01c995ad88eec9490b5cc
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-04-28 22:22:21 +0530

    [Shraddha_Singh] Refactored. Deleted extra code

--- fundoonotesui/src/App.js
+++ fundoonotesui/src/App.js
@@ -5,18 +5,9 @@ import MainPage from './Components/MainPage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component{
-
-  // constructor(props) {
-  //   super(props)
-  // }
-
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path='/' component={MainPage} exact />
-        </Switch>
-      </Router>
+      < MainPage />
     )
   }
 }
