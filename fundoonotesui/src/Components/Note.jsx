74ec8fbeb6929a8a0c40072fa8b85e42bd265e40
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-05-02 20:03:12 +0530

    [Shraddha_Singh] Refactored. Edited code to remove dialog box

--- fundoonotesui/src/Components/Note.jsx
+++ fundoonotesui/src/Components/Note.jsx
@@ -1,112 +1,59 @@
 import React, { Component } from 'react';
-import NoteCard from './NoteCard';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import {AddAlert, AddAPhoto, ColorLens, AddCircleOutline}from '@material-ui/icons';
 
 export default class Note extends Component{
-
-    state = {
-        open: false,
-        title: '',
-        note: '',
-        notes: []
-    }
-
-    handleClickOpen = async () => {
-        const doesOpen = this.state.open;
-        await this.setState({
-            open: !doesOpen
-        })
-    };
-
-    handleClose = async () => {
-        const doesOpen = this.state.open
-        await this.setState({
-            open: !doesOpen
-        })
-        console.log(this.state.open);
-    };
-
-    titleChangeHandler = (event) => {
-        const title = event.target.value;
-        this.setState({
-            title: title
-        })
-
-    }
-
-    noteChangeHandler = (event) => {
-        const note = event.target.value;
-        this.setState({
-            note: note
-        })
-
-    }
-
-    saveChangeHandler = () => {
-        const title = this.state.title;
-        const note = this.state.note;
-        const doesOpen = this.state.open;
-        const noteObj = {
-            title: title,
-            note: note
-        }
-        const notes = this.state.notes;
-        notes.push(noteObj);
-        this.setState({
-            notes: [...notes],
-            open: !doesOpen
-        })
-    }
 
     render(){
         return (
             <>
                 <div className='note-div'>
-                    <Button id='note-button' startIcon={<NoteAddIcon/>}   onClick={this.handleClickOpen}>
-                        Take a Note ...
-                    </Button>
-                </div>
-                    <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                    <DialogTitle id="form-dialog-title">Note</DialogTitle>
-                    <DialogContent>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Title"
-                            fullWidth
-                            onChange={this.titleChangeHandler}
+                    <div>
+                    <TextField className='take-note' id="standard-basic" style={{display:  this.props.showField ? 'none' : 'block' }} onClick={this.props.showFieldHandler} placeholder='Take Note...' />
+                    {
+                        this.props.showField ?<div className='title-note-div'>
+                        <TextField id="standard-basic" placeholder="Title" onChange={this.props.titleChangeHandler} />
+                        <TextField id="standard-basic" placeholder="Write Here.."  
+                        multiline
+                        rows={10} 
+                        onChange={this.props.noteChangeHandler}    
                         />
-                    </DialogContent>
-                    
-                    <DialogContent>
-                        <TextField
-                            autoFocus
-                            multiline
-                            rows={10}
-                            placeholder="Write Here..."
-                            fullWidth
-                            onChange={this.noteChangeHandler}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant='contained' onClick={this.handleClose} color="black">
-                            Cancel
-                        </Button>
-                        <Button variant='contained' onClick={this.saveChangeHandler} color="black">
-                            Save
-                            </Button>
-                    </DialogActions>
-                   
-                </Dialog>
+                        <div className='note-btn-div'>
+                            <Button 
+                                variant = 'outlined'
+                                color='default'
+                                startIcon ={<AddCircleOutline/>}
+                                onClick = {this.props.saveChangeHandler}
+                                /> 
+                                <Button 
+                                variant = 'outlined'
+                                color='default'
+                                startIcon ={<AddAlert/>}
+                                />
+                                <Button 
+                                variant='outlined'
+                                color="default"
+                                startIcon={<AddAPhoto />}
+                                />
+                                <Button 
+                                variant='outlined'
+                                color="default"
+                                startIcon={<ColorLens />}
+                                />                    
+                                <Button 
+                                variant='outlined'
+                                color="default" onClick={this.props.handleClose}>
+                                Close
+                                </Button>
+                        </div>
+                        </div>
+                         : null 
+                        }
+                      </div>
+                    </div>  
+
                 
-                <NoteCard notes={this.state.notes}/>
             </>
         )
     }
