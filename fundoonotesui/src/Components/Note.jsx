73d799bd3b9c335eec0b7ea2a3d1b3c1b6a367d4
shraddhavatscse@gmail.com <shraddhavatscse@gmail.com>
2020-05-03 16:59:04 +0530

    [Shraddha_Singh] Refactored. Added clickaway listener in the main div

--- fundoonotesui/src/Components/Note.jsx
+++ fundoonotesui/src/Components/Note.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import {AddAlert, AddAPhoto, ColorLens, AddCircleOutline}from '@material-ui/icons';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 
 export default class Note extends Component{
 
@@ -10,9 +11,12 @@ export default class Note extends Component{
             <>
                 <div className='note-div'>
                     <div>
+                    
                     <TextField className='take-note' id="standard-basic" style={{display:  this.props.showField ? 'none' : 'block' }} onClick={this.props.showFieldHandler} placeholder='Take Note...' />
                     {
-                        this.props.showField ?<div className='title-note-div'>
+                        this.props.showField ?
+                        <ClickAwayListener onClickAway={this.props.saveChangeHandler}>
+                        <div className='title-note-div'>
                         <TextField id="standard-basic" placeholder="Title" onChange={this.props.titleChangeHandler} />
                         <TextField id="standard-basic" placeholder="Write Here.."  
                         multiline
@@ -20,6 +24,7 @@ export default class Note extends Component{
                         onChange={this.props.noteChangeHandler}    
                         />
                         <div className='note-btn-div'>
+                            <div>
                             <Button 
                                 variant = 'outlined'
                                 color='default'
@@ -40,14 +45,18 @@ export default class Note extends Component{
                                 variant='outlined'
                                 color="default"
                                 startIcon={<ColorLens />}
-                                />                    
+                                />  
+                                </div> 
+                                <div className='sub-btn-div'>                 
                                 <Button 
                                 variant='outlined'
                                 color="default" onClick={this.props.handleClose}>
                                 Close
                                 </Button>
+                                </div>
                         </div>
                         </div>
+                        </ClickAwayListener>
                          : null 
                         }
                       </div>
